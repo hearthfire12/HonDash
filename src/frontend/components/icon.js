ec656d8e1cdc8e93f05a3da3a024a09c97dfee0b
hearthfire12 <solomoychenkoo@gmail.com>
2023-08-13 16:26:53 +0300

    Update icon.js

--- src/frontend/components/icon.js
+++ src/frontend/components/icon.js
@@ -1,17 +1,19 @@
 class Icon {
   constructor(args) {
     this.element = document.getElementById(args.id);
-    this.pathOff = "default_off.svg";
-    this.pathOn = "";
+    this.pathOff = args.pathOff || "";
+    this.pathOn = args.pathOff || "";
     var img = document.createElement("img");
     img.src = "icons/" + this.pathOff;
     img.style.width = "100%";
+    img.style.transform = `scale(${args.scale || 1})`;
     this.element.appendChild(img);
   }
 
   refresh(value) {
     var img = document.createElement("img");
     img.style.width = "100%";
+    img.style.transform = `scale(${args.scale || 1})`;
     img.src = "icons/" + (value > 0 ? this.pathOn : this.pathOff);
     this.element.innerHTML = "";
     this.element.appendChild(img);
